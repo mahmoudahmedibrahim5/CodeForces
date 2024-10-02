64050d6835bde1c8ae7692b10c8363f46e86b7c2
Mahmoud Ahmed <mahmoudahmedibrahim5@gmail.com>
2024-10-02 11:46:05 +0300

    Accepted

---
+++ 4-1200/Game.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    unsigned long long n;
+    cin >> n;
+    if(n % 2 == 0)
+        cout << "2" << endl;
+    else
+        cout << "1" << endl;
+}
