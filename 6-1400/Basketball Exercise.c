03b6f92391292c21c5223fae5686c2772fd9e6ed
Mahmoud Ahmed <mahmoudahmedibrahim5@gmail.com>
2025-01-27 14:37:33 +0200

    Accepted

--- 6-1400/Basketball Exercise.c
+++ 6-1400/Basketball Exercise.c
@@ -1,113 +1,52 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-unsigned long long findResult(unsigned long long result, int t, unsigned long long** cmp, unsigned long long* row1, unsigned long long* row2, int n)
-{
-    //printf("Initial result = %d\n", result);
-    for(int i = 1; i < n - 1; i++){
-        if(t == 1)
-        {
-            if(cmp[i][1] > cmp[i + 1][1]){
-                result += row2[i];
-                //printf("Row2 chosen\n");
-                t = 0;
-            }
-            else
-                t = 3;
-        }
-        else if(t == 3)
-        {
-            if(cmp[i][1] > cmp[i + 1][1]){
-                result += row2[i];
-                //printf("Row2 chosen\n");
-                t = 0;
-            }
-            else{
-                result += row2[i - 1];
-                //printf("Row2 chosen\n");
-                result += row1[i];
-                //printf("Row1 chosen\n");
-                t = 1;
-            }
-        }
-
-        else if(t == 0)
-        {
-            if(cmp[i][0] > cmp[i + 1][0]){
-                result += row1[i];
-                //printf("Row1 chosen\n");
-                t = 1;
-            }
-            else
-                t = 2;
-        }
-        else if(t == 2)
-        {
-            if(cmp[i][0] > cmp[i + 1][0]){
-                result += row1[i];
-                //printf("Row1 chosen\n");
-                t = 1;
-            }
-            else
-            {
-                result += row1[i - 1];
-                //printf("Row1 chosen\n");
-                result += row2[i];
-                //printf("Row2 chosen\n");
-                t = 0;
-            }
-        }
-        //printf("Result = %d\n", result);
-    }
-    if(n != 1)
-    {
-        if(t == 1 || t == 3)
-            result += row2[n - 1];
-        else
-            result += row1[n - 1];
-    }
-    return result;
-}
+typedef unsigned long long uint64_t;
 
 int main()
 {
     int n;
     scanf("%d", &n);
 
-    unsigned long long* row1 = (unsigned long long *)malloc(sizeof(unsigned long long) * n);
-    unsigned long long* row2 = (unsigned long long *)malloc(sizeof(unsigned long long) * n);
+    uint64_t* row1 = (uint64_t *)malloc(sizeof(uint64_t) * n);
+    uint64_t* row2 = (uint64_t *)malloc(sizeof(uint64_t) * n);
 
     for(int i = 0; i < n; i++)
-        scanf("%ld", &row1[i]);
+        scanf("%llu", &row1[i]);
     
     for(int i = 0; i < n; i++)
-        scanf("%ld", &row2[i]);
+        scanf("%llu", &row2[i]);
 
-    unsigned long long** cmp = (unsigned long long **)malloc(sizeof(unsigned long long *) * n);
+    uint64_t** cmp = (uint64_t **)malloc(sizeof(uint64_t *) * 2);
+    cmp[0] = (uint64_t *)malloc(sizeof(uint64_t) * n);
+    cmp[1] = (uint64_t *)malloc(sizeof(uint64_t) * n);
 
-    cmp[n - 1] = (unsigned long long *)malloc(sizeof(unsigned long long) * 2);
-    cmp[n - 1][0] = row1[n - 1];
-    cmp[n - 1][1] = row2[n - 1];
+    uint64_t first = 0, second = 0;
+    for(int i = 0; i < n; i++)
+    {
+        if(first + row2[i] > second)
+            cmp[0][i] = first + row2[i];
+        else
+            cmp[0][i] = second;
 
-    for(int i = n - 2; i >= 0; i--){
-        cmp[i] = (unsigned long long *)malloc(sizeof(unsigned long long) * 2);
-        cmp[i][0] = row1[i] + cmp[i + 1][1];
-        cmp[i][1] = row2[i] + cmp[i + 1][0];
+        if(second + row1[i] > first)
+            cmp[1][i] = second + row1[i];
+        else
+            cmp[1][i] = first;
+
+        first = cmp[1][i];
+        second = cmp[0][i];
     }
 
-    unsigned long long result1 = findResult(row1[0], 1, cmp, row1, row2, n);
-    unsigned long long result2 = findResult(row2[0], 0, cmp, row1, row2, n);
-    //printf("Result1 = %d, Result2 = %d\n", result1, result2);
-
-    if(result1 > result2)
-        printf("%ld\n", result1);
+    if(cmp[0][n - 1] > cmp[1][n - 1])
+        printf("%llu\n", cmp[0][n - 1]);
     else
-        printf("%ld\n", result2);
+        printf("%llu\n", cmp[1][n - 1]);
 
     free(row1);
     free(row2);
-    for(int i = 0; i < n; i++)
-        free(cmp[i]);
+    free(cmp[0]);
+    free(cmp[1]);
     free(cmp);
     return 0;
 }
