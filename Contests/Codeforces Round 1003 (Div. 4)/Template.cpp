38e209bbdaa50163b799a5dbe760e2f7db8ed802
Mahmoud Ahmed <mahmoudahmedibrahim5@gmail.com>
2025-02-09 22:24:04 +0200

    Template

---
+++ Contests/Codeforces Round 1003 (Div. 4)/Template.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+
+using namespace std;
+
+typedef unsigned long long uint64_t;
+typedef long long int64_t;
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        
+    }
+    
+
+    return 0;
+}
