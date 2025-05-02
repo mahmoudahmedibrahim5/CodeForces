385ee2bc442aacca31c536e8281221074ec0870b
Mahmoud Ahmed <mahmoudahmedibrahim5@gmail.com>
2025-05-02 12:00:22 +0300

    Accepted (Better Solution)

--- 6-1400/Queen.cpp
+++ 6-1400/Queen.cpp
@@ -5,48 +5,36 @@
 
 using namespace std;
 
-struct vertex{
+struct vertex
+{
     vertex* parent;
     vector<vertex*> children;
-    int childrenCount;
-
-    int value;
     bool state;
-
-    // vertex(){
-    //     parent = NULL;
-    //     childrenCount = 0;
-    // }
 };
 
 void addChild(vertex* parent, vertex* child)
 {
-    parent->childrenCount++;
     parent->children.push_back(child);
     child->parent = parent;
 }
 
-vector<vertex*> d;
-
-void searchForDelete(vertex* head)
+void searchForDelete(vertex* arr, int n, vector<int>& result)
 {
-    if(head->childrenCount == 0)
-        return;
-    
-    for(int i = 0; i < head->childrenCount; i++)
+    for(int i = 0; i < n; i++)
     {
-        if(head->children[i]->state == 1)
+        if(arr[i].state && arr[i].parent != NULL)
         {
             bool del = true;
-            for(int j = 0; j < head->children[i]->childrenCount; j++)
-                if(head->children[i]->children[j]->state == 0){
+            for(int j = 0; j < arr[i].children.size(); j++)
+            {
+                if(!arr[i].children[j]->state){
                     del = false;
                     break;
                 }
+            }
             if(del)
-                d.push_back(head->children[i]);
+                result.push_back(i + 1);
         }
-        searchForDelete(head->children[i]);
     }
 }
 
@@ -56,35 +44,25 @@ int main()
     cin >> n;
 
     vertex* arr = new vertex[n];
-    for(int i = 0; i < n; i++){
-        arr[i].childrenCount = 0;
-        arr[i].parent = NULL;
-        arr[i].value = i + 1;
-    }
-
-    int head;
     int p, c;
     for(int i = 0; i < n; i++)
     {
         cin >> p >> c;
         arr[i].state = c;
         if(p == -1)
-            head = i;
+            arr[i].parent = NULL;
         else
             addChild(&arr[p - 1], &arr[i]);
     }
 
-    searchForDelete(&arr[head]);
-
     vector<int> result;
-    for(int i = 0; i < (int)d.size(); i++)
-        result.push_back(d[i]->value);
+    searchForDelete(arr, n, result);
 
     if(result.empty())
-        cout << "-1";
-    
-    sort(result.begin(), result.end());
-    for(int i = 0; i < (int)result.size(); i++)
-        cout << result[i] << " ";
-    cout << endl;
+        cout << "-1" << endl;
+    else
+    {
+        for(int i = 0; i < (int)result.size(); i++)
+            cout << result[i] << ((i != result.size() - 1)? " " : "\n");
+    }
 }
